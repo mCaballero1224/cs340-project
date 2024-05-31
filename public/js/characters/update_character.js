daae1ab1c36b3da0c44982b34274e6d4efdc1519
Michael Caballero <caballmi@oregonstate.edu>
2024-05-30 21:05:00 -0700

    Make 'edit' button on character table auto-fill with selected character data

--- public/js/characters/update_character.js
+++ public/js/characters/update_character.js
@@ -17,7 +17,7 @@ updateCharacterForm.addEventListener("submit", function(e) {
   let inputMagic = document.getElementById("update-character-magic");
   let inputHealth = document.getElementById("update-character-health");
 
-  // get teh values from the form fields
+  // get the values from the form fields
   let characterIdValue = inputCharacterId.value;
   let playerIdValue = inputPlayerId.value;
   let nameValue = inputName.value;
@@ -58,6 +58,36 @@ updateCharacterForm.addEventListener("submit", function(e) {
     }
   };
 });
+
+function setupCharacterEdit(characterId) {
+  showUpdateForm();
+ // get the form fields we need to enter data into 
+  let inputCharacterId = document.getElementById("mySelect");
+  let inputPlayerId = document.getElementById("update-player-id");
+  let inputName = document.getElementById("update-character-name");
+  let inputLevel = document.getElementById("update-character-level");
+  let inputExperience = document.getElementById("update-character-experience");
+  let inputAgility = document.getElementById("update-character-agility");
+  let inputStrength = document.getElementById("update-character-strength");
+  let inputMagic = document.getElementById("update-character-magic");
+  let inputHealth = document.getElementById("update-character-health");
+
+
+  let table = document.querySelector("table tbody");
+  for (let i = 0, row; row = table.rows[i]; i++) {
+   if (table.rows[i].getAttribute("data-value") == characterId) {
+     inputCharacterId.value = row.cells[2].innerText;
+     inputPlayerId.value = row.cells[3].innerText;
+     inputName.value = row.cells[4].innerText;
+     inputLevel.value = row.cells[5].innerText;
+     inputExperience.value = row.cells[6].innerText;
+     inputAgility.value = row.cells[7].innerText;
+     inputStrength.value = row.cells[8].innerText;
+     inputMagic.value = row.cells[9].innerText;
+     inputHealth.value = row.cells[10].innerText;
+   }
+  }
+};
 
 
 function updateRow(characterIdvalue) {
